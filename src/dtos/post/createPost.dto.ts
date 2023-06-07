b1029b204e699b4ebe1c1828fbc665fa2e4e741c
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2023-06-07 08:48:27 -0300

    createPost implementado

--- src/dtos/post/createPost.dto.ts
+++ src/dtos/post/createPost.dto.ts
@@ -10,4 +10,4 @@ export type CreatePostOutputDTO = undefined
 export const CreatePostSchema = z.object({
     name: z.string().min(1),
     token: z.string().min(1)
-})
+}).transform(date => date as CreatePostInputDTO)
